44d742d678071a1f3aea8eaccee527b1f517f694
Carter Kelly <ckelly919@hotmail.com>
2017-11-05 20:45:49 -0800

    users/sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get 'signup' => 'users#new'
+  resources :users
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
