aaf2ced10b385d56778823dd183e0ddc2b60035d
Phillip Dykman <pdykman@sfu.ca>
2017-11-19 23:36:50 -0800

    test login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,4 +4,21 @@ class SessionsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @password = 'testpassword'
+    @user = User.create(name: 'testuser', email: 'testemail@test.com', password: @password)
+  end
+
+  def teardown
+    @user.destroy
+  end
+
+  test "log in" do
+    get	:new
+    assert_response :success
+
+    get :create, {session:{email: @user.email, password: @password}}
+    assert_redirected_to root_path
+
+  end
 end
