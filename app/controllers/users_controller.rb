539d42149a7a4176ad1ac7d9e431b09c50966ea4
Phillip Dykman <pdykman@sfu.ca>
2017-11-20 18:01:53 -0800

    added deletion of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -14,12 +15,30 @@ class UsersController < ApplicationController
     else
      render :new
      failed_login = true
+    end
+ end
+
+ def destroy
+   get_session_user
+
+   user = User.find(params[:id])
+
+   if @current_user.is_admin && @current_user != user
+     user.destroy
    end
+
+     redirect_to root_path
  end
 
  private
+
  def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+ end
+
+ def get_session_user
+   require_user
+   current_user
+ end
 
 end
