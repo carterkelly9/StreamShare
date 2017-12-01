9f7cbed4284d4e9d0e93fd22e16f5058270696d3
mthirim <myathirimay@gmail.com>
2017-12-01 15:15:23 -0800

    added some test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,32 @@ class UserTest < ActiveSupport::TestCase
       assert_not Admin.exists?(users(:user))
   end
 
+  test 'valid user' do
+  	user = User.new(name: 'test', email: 'test@test.com', password: BCrypt::Password.create('testtest', cost: 5))
+  	assert user.valid?
+  end
+
+  test 'invalid without name' do
+  end
+
+  test 'invalid without email' do
+  end
+
+  test 'invalid without password' do
+  end
+
+  test 'email is unique' do
+   	user = User.new(name: 'test', email: 'test@test.com', password: BCrypt::Password.create('testtest', cost: 5))
+   	user.save
+   	user2 = User.new(name: 'test', email: 'test@test.com', password: BCrypt::Password.create('testtest', cost: 5))
+   	assert_not user2.valid?
+  end
+
+  test 'password is invalid' do 
+  end
+
+  test 'name is invalid' do
+  end
+
+  
 end
