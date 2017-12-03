345d8e5ec57b06a994673d6767358140cd22f1c1
Phillip Dykman <pdykman@sfu.ca>
2017-12-02 16:22:39 -0800

    cascade deletion
    fixes dangling shares, media and invites

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,6 @@
 class Medium < ActiveRecord::Base
+  before_destroy :delete_shares_and_invites
+
   belongs_to :user
   has_one :upload
 
@@ -6,5 +8,11 @@ class Medium < ActiveRecord::Base
   def shared_users
     shares = Share.where(owner_id: self.user_id, medium_id: self.id)
     return User.find(shares.map(&:guest_id))
+  end
+
+  private
+  def delete_shares_and_invites
+    Share.where(medium_id: id).destroy_all
+    Invite.where(medium_id: id).destroy_all
   end
 end
